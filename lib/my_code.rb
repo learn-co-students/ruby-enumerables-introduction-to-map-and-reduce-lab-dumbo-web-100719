b7e6f3955fa527c07dad13c5dbf70017b3827d85
Stephen Keating <stephen.keating@gmail.com>
2019-09-29 11:38:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 # My Code here....
 def map_to_negativize(source_array)
-  counter = 0
+  i = 0
  
-  while counter < source_array.length do
-    source_array[counter] *= -1 
-    counter += 1
+  while i < source_array.length do
+    source_array[i] *= -1 
+    i += 1
   end
   source_array
 end
@@ -14,48 +14,48 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  counter = 0
+  i = 0
  
-  while counter < source_array.length do
-    source_array[counter] *= 2 
-    counter += 1
+  while i < source_array.length do
+    source_array[i] *= 2 
+    i += 1
   end
   source_array
 end
 
 def map_to_square(source_array)
-  counter = 0
+  i = 0
  
-  while counter < source_array.length do
-    source_array[counter] **= 2
-    counter += 1
+  while i < source_array.length do
+    source_array[i] **= 2
+    i += 1
   end
   source_array
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  counter = 0
+  i = 0
   total = starting_point
   
-  while counter < source_array.length do
-    total = total + source_array[counter]
-    counter += 1
+  while i < source_array.length do
+    total = total + source_array[i]
+    i += 1
   end
   total
 end
 
 def reduce_to_all_true(source_array)
-  counter = 0
+  i = 0
  
-  while counter < source_array.length do
-    if source_array[counter] == true
-      source_array[counter] = true
+  while i < source_array.length do
+    if source_array[i] == true
+      source_array[i] = true
     end
-    break if source_array[counter] == false
-    counter += 1
+    break if source_array[i] == false
+    i += 1
   end
   
-  if source_array[counter] == false
+  if source_array[i] == false
     source_array = false
   end
   
@@ -63,17 +63,17 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  counter = 0
+  i = 0
  
-  while counter < source_array.length do
-    if source_array[counter] == true
-      source_array[counter] = true
+  while i < source_array.length do
+    if source_array[i] == true
+      source_array[i] = true
     end
-    break if source_array[counter] == true
-    counter += 1
+    break if source_array[i] == true
+    i += 1
   end
   
-  if source_array[counter] == true
+  if source_array[i] == true
     source_array = true
   else
     source_array = false
