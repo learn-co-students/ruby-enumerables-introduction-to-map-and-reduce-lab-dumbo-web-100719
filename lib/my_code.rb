b2c379789e34ac441816ba81e2fe3d49e3dbbf8f
Muna Abdelrazeq <mma207@scarletmail.rutgers.edu>
2019-09-29 20:58:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,64 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do 
+    sources = source_array[i] * -1 
+    new_array.push(sources)
+    i += 1
+  end
+  return new_array
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+    i = 0 
+  new_array = []
+  while i < source_array.length do 
+    sources = source_array[i] * 2 
+    new_array.push(sources)
+    i += 1
+  end
+  return new_array
+end
+
+def map_to_square(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do 
+    sources = source_array[i] ** 2 
+    new_array.push(sources)
+    i += 1
+  end
+  return new_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0 
+  while i < source_array.length do 
+    total += source_array[i] 
+    i += 1
+  end
+  return total 
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i] 
+    i += 1
+  end 
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i] 
+    i += 1
+  end 
+  return false
+end
