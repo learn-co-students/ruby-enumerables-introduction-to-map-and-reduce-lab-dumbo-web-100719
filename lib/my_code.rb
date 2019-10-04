bf0ef03e92c76f2da1993e824d64a07de9d84507
George Skrelja <gskrelja@gmail.com>
2019-10-04 05:12:10 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,8 @@
 # My Code here....
 def map_to_negativize(source_array)
-#  source_array.map { |i|
-#    i * -1
-#  }
+  # source_array.map { |i|
+  #   i * -1
+  # }
   new_array = []
   source_array.length.times do |i|
     new_array.push(source_array[i] * -1)
@@ -15,9 +15,9 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-#  source_array.map { |i|
-#    i * 2
-#  }
+  # source_array.map { |i|
+  #   i * 2
+  # }
   new_array = []
   source_array.length.times do |i|
     new_array.push(source_array[i] * 2)
@@ -26,9 +26,9 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-#  source_array.map { |i|
-#    i * i
-#  }
+  # source_array.map { |i|
+  #  i * i
+  # }
   new_array = []
   source_array.length.times do |i|
     new_array.push(source_array[i] ** 2)
@@ -37,19 +37,32 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-#  source_array.reduce(:+) + starting_point
-  total = 0
-  source_array.length.times do |i|
-    total += source_array[i]
+  # source_array.reduce(:+) + starting_point
+  total = starting_point
+  counter = 0
+  while counter < source_array.length do
+    total += source_array[counter]
+    counter += 1
   end
-  total + starting_point
+  total
 end
 
 def reduce_to_all_true(source_array)
-  source_array.all?
-
+  # source_array.all?
+  counter = 0
+  while counter < source_array.length do
+    return false if source_array[counter] == false
+    counter += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
-  source_array.any?
+  # source_array.any?
+  counter = 0
+  while counter < source_array.length do
+    return true if source_array[counter] == true
+    counter += 1
+  end
+  return false
 end
