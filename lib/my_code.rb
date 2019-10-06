04802578527f76c56471f63f8b0d5bba729cd34e
Dwayne McFarlane <dwaynemcfarlaneny@gmail.com>
2019-10-06 02:52:43 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,27 @@
-# My Code here....
+my_array = [1,2,3,4,5]
+
+def map_to_negativize(source_array)\
+neg_num = my_array*-1
+return neg_num
+end
+
+def map_to_no_change(source_array)
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
+
+def reduce_to_total(source_array, starting_point)
+end
+
+def map_to_no_change(source_array)
+end
+
+def map_to_double(source_array)
+end
+
+def map_to_square(source_array)
+end
